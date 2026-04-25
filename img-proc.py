2584434021fe986cdbddd960021d03fdcc0a57bb
Marina <140115877+mhmarina@users.noreply.github.com>
2026-04-24 17:07:10 -0700

    updated dataset

--- img-proc.py
+++ img-proc.py
@@ -3,11 +3,15 @@ import os
 
 input_dir = "data-raw/"
 output_dir = "data-preproc/"
+# rename_dir = "data-rename/"
 os.makedirs(output_dir, exist_ok=True)
 
 for i, filename in enumerate(os.listdir(input_dir)):
     img = Image.open(os.path.join(input_dir, filename))
     name = f"{i}.jpg"
+    # if(filename.endswith("png")):
+    #     name = f"{i}.png"
+    # img.save(os.path.join(rename_dir, name))
     img = img.convert("L")          
     img = img.resize((64, 64), Image.LANCZOS)  
     img.save(os.path.join(output_dir, name))
